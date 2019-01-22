f93e84712eb56f77c2c3e3ee907567c1d44b246f
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-22 01:27:40 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,18 +10,18 @@ def argue(argument)
   argument
 end
 
-def greeting
+def greeting(greeting, name)
   
 end
 
-def return_a_value
-
+def return_a_value(phrase = 'Nice')
+  phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = 'expert')
+  phrase
 end
 
-def pizza_party
-
+def pizza_party(topping = 'cheese')
+  topping
 end
